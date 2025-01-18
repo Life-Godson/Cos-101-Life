8b01c8c0f2b4cd6697f6e3c26b7e5b181c7e32c5
Life-Godson <lifechidiebere@gmail.com>
2025-01-18 07:18:19 -0800

    Add files via upload

---
+++ COS101.py
@@ -0,0 +1,36 @@
+def a():
+    oscillation = float(input("how many oscillations were there"))
+    time = float(input("how long did it take?"))
+    print(oscillation/time)
+
+
+a()
+
+def b():
+    #  speed = distance / time
+    distance = float (input("value of the distance?"))
+    time = float(input("value of time taken"))
+    print(distance/time)
+
+b()
+
+def c():
+    mass = float(input("input mass?"))
+    velocity = float(input("input velocity"))
+    print(mass*velocity)
+
+c()
+
+def d():
+    mass = float(input("input mass?"))
+    acceleration = float(input("input acceleration?"))
+    print(mass*acceleration)
+
+d()
+
+def e():
+    power = float(input("what is the power"))
+    time = float(input("what is the time"))
+    print(power*time)
+
+e()
